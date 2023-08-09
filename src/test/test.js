bffe12f1ad22658b0dbe0d09214d8b8d90ef51dd
Demonik993 <wiecek.dom@gmail.com>
2023-08-09 16:57:25 +0200

    fetching json obj from netlify

--- src/test/test.js
+++ src/test/test.js
@@ -166,7 +166,7 @@ function loadtest (userData) {
             }
         });
         // fetch answers from json
-        fetch('https://katarzynas.netlify.app/src/test/test-answer.json')
+        fetch('./test-answer.json')
             .then(response => {
                 if(!response.ok){
                     throw new Error(`HTTP error: ${response.status}`);
