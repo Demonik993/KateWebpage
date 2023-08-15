7c4343486bbf9b96ce62e2bfa1222d09022e3217
Demonik993 <wiecek.dom@gmail.com>
2023-08-15 21:43:59 +0200

    modified main page css file and some changes in search div

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-// document.onload = alert(`Kasia, zaczynamy pracować nad Twoją stroną! Narazie wersja wyłacznie na ekrany pow 600px. Musimy zastanowić się nad kolorystyką.`);
+const search = document.querySelector('#searching')
 
+//SERCHING WEBSITE
+search.onsubmit = async (e) => {
+    e.preventDefault();
+    console.log("HELLO WORLD")
 
-const logo = document.querySelector('#logo');
-const boxLogo = document.querySelector('#text-box');
-const webhead = document.querySelector('#name');
-const boxName = document.querySelector('#text-name');
-const navPan = document.querySelector('#nav-pan');
-const boxNav = document.querySelector('#text-nav');
-
-function textBox(element, box){
-    element.addEventListener('mouseover', (e)=>{
-        const x = e.clientX;
-        const y = e.clientY;
-        box.style.display = 'flex';
-        box.style.top = `${y}px`;
-        box.style.left = `${x}px`;
-        element.addEventListener('mouseout', ()=>{
-            box.style.display = "none";
-        });
-    });
+    const input = document.querySelector("#search").value;
+    console.log(input)
 }
- textBox(logo,boxLogo);
- textBox(webhead,boxName);
- textBox(navPan,boxNav);
-
